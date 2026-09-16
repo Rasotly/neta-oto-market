83aca8057127de4739ff13eaaaf61e313601eb47
CAGRI\seker <sekercicagri41@gmail.com>
2026-09-16 17:18:10 +0300

    new migrations

---
+++ backend/NetaOtoMarket.API/Migrations/20260916141226_AddDiscountedPrice.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetaOtoMarket.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscountedPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountedPrice",
+                table: "Products",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountedPrice",
+                table: "Products");
+        }
+    }
+}
